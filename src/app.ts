9548a3a19c2f2bc1e41a19ae5533f62216cb1743
hammad-ahmed-01 <bosshammad0@gmail.com>
2026-09-02 04:44:44 +0500

    added default export of the app for prod

--- src/app.ts
+++ src/app.ts
@@ -85,3 +85,7 @@ export function createApp() {
 
   return app;
 }
+
+const app = createApp();
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,24 @@
-import { createApp } from "./app";
+import app from "./app";
 import { logger } from "./common/utils/logger";
 import { env } from "./config/env";
 
-const app = createApp();
-
-const server = app.listen(env.PORT, () => {
-  logger.info(
-    { port: env.PORT, env: env.NODE_ENV },
-    "MeterStack API listening",
-  );
-});
-
-function shutdown(signal: string) {
-  logger.info({ signal }, "Shutting down");
-  server.close(() => {
-    process.exit(0);
+if (!process.env.VERCEL) {
+  const server = app.listen(env.PORT, () => {
+    logger.info(
+      { port: env.PORT, env: env.NODE_ENV },
+      "MeterStack API listening",
+    );
   });
+
+  function shutdown(signal: string) {
+    logger.info({ signal }, "Shutting down");
+    server.close(() => {
+      process.exit(0);
+    });
+  }
+
+  process.on("SIGINT", () => shutdown("SIGINT"));
+  process.on("SIGTERM", () => shutdown("SIGTERM"));
 }
 
-process.on("SIGINT", () => shutdown("SIGINT"));
-process.on("SIGTERM", () => shutdown("SIGTERM"));
+export default app;
